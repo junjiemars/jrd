63d150e54c0003bad95b9edccdd718c4a8f64034
南山 <junjiemars@gmail.com>
2014-09-15 13:20:32 +0800

    try run it on windows 7

--- src/java/jrd/src/com/xws/JRD.java
+++ src/java/jrd/src/com/xws/JRD.java
@@ -1,9 +1,10 @@
 package com.xws;
+
 import static java.lang.System.out;
 
 public final class JRD {
   public static final void main(String[] args) {
-	out.println("Redis Demo");
+    out.println("Redis Demo");
 
     Simple.run();
   }
